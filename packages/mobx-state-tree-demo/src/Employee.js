ff2a5be621ddb9c6e657e835829015486a4d7f2d
Igor Soloydenko <igor.soloydenko@palador.com>
2018-09-24 16:16:59 -0700

    mobx-tree-state demo to operate on 20 fields + 20 computed fields per object

--- packages/mobx-state-tree-demo/src/Employee.js
+++ packages/mobx-state-tree-demo/src/Employee.js
@@ -2,38 +2,71 @@ import React, { Component } from 'react';
 import './Employee.css';
 
 class Employee extends Component {
-  renderField(field, index) { return <span key={index}>{field.k} {this.props.value[field.v]}</span>; }
+  renderField(field, index) { return <span key={index} title={field.k}>{field.k} {this.props.value[field.v]}</span>; }
 
   render() {
     return (
       <div className="employee-card">
         <div>
-          <h4>Last Pay Change {this.props.value.payEffectiveDate}</h4>
           <h2>{this.props.value.firstName} {this.props.value.lastName}</h2>
         </div>
-        <div>
-          <div>
-            <span>Current range {this.props.value.getCurrentPay()}</span>
-            <span>Prorated {this.props.value.getProratedPay()}</span>
-          </div>
-          <div>
+        
+        <div className="computed">
+          <span><strong>Computed</strong></span>|
+          <span title="getFullName1()">{this.props.value.getFullName1()}</span>|
+          <span title="getFullName2()">{this.props.value.getFullName2()}</span>|
+          <span title="getHireDateHuman()">{this.props.value.getHireDateHuman()}</span>|
+          <span title="getPayEffectiveDateHuman()">{this.props.value.getPayEffectiveDateHuman()}</span>|
+          <span title="getCurrentPay()">{this.props.value.getCurrentPay()}</span>|
+          <span title="getProratedPay()">{this.props.value.getProratedPay()}</span>|
+          <span title="getOrg()">{this.props.value.getOrg()}</span>|
+          <span title="getCountryInfo()">{this.props.value.getCountryInfo()}</span>|
+          <span title="getManager()">{this.props.value.getManager()}</span>|
+          <span title="getPayInfo()">{this.props.value.getPayInfo()}</span>|
+          <span title="getOther()">{this.props.value.getOther()}</span>|
+        </div>
+
+        <div className="computed">
+          <span><strong>Computed-reversed</strong></span>|
+          <span title="getFullName1()">{this.props.value.getFullName1Rev()}</span>|
+          <span title="getFullName2()">{this.props.value.getFullName2Rev()}</span>|
+          <span title="getHireDateHuman()">{this.props.value.getHireDateHumanRev()}</span>|
+          <span title="getPayEffectiveDateHuman()">{this.props.value.getPayEffectiveDateHumanRev()}</span>|
+          <span title="getCurrentPay()">{this.props.value.getCurrentPayRev()}</span>|
+          <span title="getProratedPay()">{this.props.value.getProratedPayRev()}</span>|
+          <span title="getOrg()">{this.props.value.getOrgRev()}</span>|
+          <span title="getCountryInfo()">{this.props.value.getCountryInfoRev()}</span>|
+          <span title="getManager()">{this.props.value.getManagerRev()}</span>|
+          <span title="getPayInfo()">{this.props.value.getPayInfoRev()}</span>|
+          <span title="getOther()">{this.props.value.getOtherRev()}</span>|
+        </div>
+
+        <div className="rendered">
+          <span><strong>Rendered</strong></span>
           {
             [
-              { k: 'Country', v: 'countryCode' },
-              { k: 'Location', v: 'jobCodeKey' },
-              { k: 'PromotionId', v: 'promotionId' },
-            ].map((field, index) => this.renderField(field, index))
-          }
-          </div>
-          <div>
-          {
-            [
-              { k: 'Department', v: 'department' },
+              { k: 'employeeSourceId', v: 'employeeSourceId' },
+              { k: 'firstName', v: 'firstName' },
+              { k: 'lastName', v: 'lastName' },
+              { k: 'payEffectiveDate', v: 'payEffectiveDate' },
+              { k: 'hireDate', v: 'hireDate' },
+              { k: 'currentPay', v: 'currentPay' },
+              { k: 'prorateRatio', v: 'prorateRatio' },
+              { k: 'department', v: 'department' },
               { k: 'businessUnit', v: 'businessUnit' },
-              { k: 'Hire Date', v: 'hireDate' },
+              { k: 'country', v: 'country' },
+              { k: 'countryCode', v: 'countryCode' },
+              { k: 'isoCurrencyCode', v: 'isoCurrencyCode' },
+              { k: 'managerSourceId', v: 'managerSourceId' },
+              { k: 'baseSalary', v: 'baseSalary' },
+              { k: 'hourlyRate', v: 'hourlyRate' },
+              { k: 'currentPayWithFte', v: 'currentPayWithFte' },
+              { k: 'jobCodeKey', v: 'jobCodeKey' },
+              { k: 'promotionId', v: 'promotionId' },
+              { k: 'isHourly', v: 'isHourly' },
+              { k: 'hasPromotionRequest', v: 'hasPromotionRequest' },
             ].map((field, index) => this.renderField(field, index))
           }
-          </div>
         </div>
       </div>
     );
--- packages/mobx-state-tree-demo/src/state-management/EmployeeModel.js
+++ packages/mobx-state-tree-demo/src/state-management/EmployeeModel.js
@@ -1,27 +1,60 @@
-import { types } from 'mobx-state-tree';
+import { types, getParent } from 'mobx-state-tree';
+
+const toSimpleName = employeeModel => `${employeeModel.firstName} ${employeeModel.lastName}`;
+
+const reverse = input => input.split('').reverse().join('');
 
 const EmployeeModel = types
   .model(
     `Employee`,
     {
-      employeeSourceId: types.number,
+      employeeSourceId: types.string,
       firstName: types.string,
       lastName: types.string,
       payEffectiveDate: types.string,
+      hireDate: types.string,
       currentPay: types.number,
-      prorateRatio: types.number
+      prorateRatio: types.number,
+      department: types.string,
+      businessUnit: types.string,
+      country: types.string,
+      countryCode: types.string,
+      isoCurrencyCode: types.string,
+      managerSourceId: types.string,
+      baseSalary: types.number,
+      hourlyRate: types.number,
+      currentPayWithFte: types.number,
+      jobCodeKey: types.string,
+      promotionId: types.string,
+      isHourly: types.boolean,
+      hasPromotionRequest: types.boolean,
     }
   )
   .views(
     employeeModel => ({
+      getProratedPay: () => (employeeModel.currentPay * employeeModel.prorateRatio).toFixed(2),
+      getCurrentPay: () => employeeModel.currentPay.toFixed(2),
+      getFullName1: () => `${employeeModel.lastName}, ${employeeModel.firstName}`,
+      getFullName2: () => toSimpleName(employeeModel),
+      getHireDateHuman: () => new Date(Date.parse(employeeModel.hireDate)).toLocaleDateString(),
+      getPayEffectiveDateHuman: () => new Date(Date.parse(employeeModel.payEffectiveDate)).toLocaleDateString(),
+      getOrg: () => `${employeeModel.department} > ${employeeModel.businessUnit}`,
+      getCountryInfo: () => `${employeeModel.country} (${employeeModel.countryCode}) ${employeeModel.isoCurrencyCode}`,
+      getManager: () => `Manager: ${toSimpleName(getParent(employeeModel, 1).find(p => p.employeeSourceId === employeeModel.managerSourceId) || {})}`,
+      getPayInfo: () => `Basic pay info: ${JSON.stringify([employeeModel.baseSalary, employeeModel.hourlyRate, employeeModel.currentPayWithFte])}`,
+      getOther: () => `${employeeModel.jobCodeKey} - ${employeeModel.promotionId} - ${employeeModel.isHourly} - ${employeeModel.hasPromotionRequest}`,
 
-      getProratedPay() {
-        return (employeeModel.currentPay * employeeModel.prorateRatio).toFixed(2);
-      },
-      getCurrentPay() {
-        return employeeModel.currentPay.toFixed(2);
-      }
-
+      getFullName1Rev: () => reverse(employeeModel.getFullName1()),
+      getFullName2Rev: () => reverse(employeeModel.getFullName2()),
+      getHireDateHumanRev: () => reverse(employeeModel.getHireDateHuman()),
+      getPayEffectiveDateHumanRev: () => reverse(employeeModel.getPayEffectiveDateHuman()),
+      getCurrentPayRev: () => reverse(employeeModel.getCurrentPay()),
+      getProratedPayRev: () => reverse(employeeModel.getProratedPay()),
+      getOrgRev: () => reverse(employeeModel.getOrg()),
+      getCountryInfoRev: () => reverse(employeeModel.getCountryInfo()),
+      getManagerRev: () => reverse(employeeModel.getManager()),
+      getPayInfoRev: () => reverse(employeeModel.getPayInfo()),
+      getOtherRev: () => reverse(employeeModel.getOther()),
     })
   );
 
